c93ef4ad222e7349e0524d8dc7aa0d0344af6bd7
Mr-reinert <reinert.neto@gmail.com>
2024-07-01 18:37:08 -0300

    implementado a ordenação alfabetica

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import  Todo from './components/Todo';
 import  TodoForm from './components/TodoForm';
 import Search from './components/Search';
+import Filter from './components/Filter';
 
 import "./App.css";
 
@@ -11,13 +12,13 @@ function App() {
 const [todos, setTodos] = useState([
   {
     id: 1,
-    text: "Criar funcionalidade X no sistema",
+    text: "Aprender a fazer uma lista de tarefas",
     category: "Trabalho",
     isCompleted: false,
   },
   {
     id: 2,
-    text:"Ir para academia",
+    text:"Estilizar tudo",
     category:"Pessoal",
     isCompleted: false,
   },
@@ -30,6 +31,9 @@ const [todos, setTodos] = useState([
   ]);
 
   const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState('All');
+  const [sort,setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -62,8 +66,23 @@ const [todos, setTodos] = useState([
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
       <div className="todo-list">
-        {todos.filter((todo)=>todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+        {todos
+        .filter((todo)=> 
+          filter === "All" 
+            ? true
+            : filter === "Completed"
+            ? todo.isCompleted
+            : !todo.isCompleted
+          )
+        .filter((todo)=>todo.text.toLowerCase().includes(search.toLowerCase()))
+        .sort((a, b)=> 
+          sort==="Asc"
+          ? a.text.localeCompare(b.text) 
+          : b.text.localeCompare(a.text)
+        )
+        .map((todo) => (
           <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,7 +7,7 @@ const Todo = ({ todo,removeTodo, completeTodo }) => {
     >
       <div className="content">
         <p>{todo.text}</p>
-        <p className="category">({todo.category})</p>
+        <p className="category">{todo.category}</p>
       </div>
       <div>
         <button className="complete" onClick={() => completeTodo(todo.id)}>Completar</button>
